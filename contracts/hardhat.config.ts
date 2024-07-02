560ab6da0230732ac5be25a25358745b14407f2b
virgil2019 <danten0402@gmail.com>
2024-07-02 23:01:09 +0800

    Read private key from file

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,8 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import fs from "fs";
+
+const sk = fs.readFileSync(".secret").toString();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    live: {
+      url: `http://127.0.0.1:8545`,
+      accounts: [sk]
+    },
+    develop: {
+      url: `http://3.236.195.117:8998`,
+      accounts: [sk]
+    }
+  }
 };
 
 export default config;
