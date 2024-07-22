90cf75149402a39ab29ae0d1794f6c939c42122c
virgil2019 <danten0402@gmail.com>
2024-07-22 15:28:31 +0800

    test: add some methods for testing

--- contracts/contracts/tests/OmniverseAABaseTest.sol
+++ contracts/contracts/tests/OmniverseAABaseTest.sol
@@ -6,6 +6,7 @@ import "../OmniverseAABase.sol";
 import "../lib/Types.sol";
 
 contract OmniverseAABaseTest is OmniverseAABase {
+    using EnumerableUTXOMap for EnumerableUTXOMap.Bytes32ToUTXOMap;
     constructor(address _sysConfig, bytes memory _AASigner, Types.UTXO[] memory _utxos, address _poseidon, address _eip712)
         OmniverseAABase(_sysConfig, _AASigner, _utxos, _poseidon, _eip712) {
     }
@@ -32,5 +33,38 @@ contract OmniverseAABaseTest is OmniverseAABase {
 
     function transfer(bytes32 assetId, Types.Output[] calldata outputs) external {
         _constructTransfer(assetId, outputs);
+    }
+
+    function getGas(Types.Output[] memory extraOutputs) external view returns (Types.Input[] memory, Types.Output[] memory) {
+        return _getGas(extraOutputs);
+    }
+
+    function updateUTXOs(bytes32 assetId) public view returns (bytes32[] memory keys, uint256 len, bytes32[] memory txids) {
+        EnumerableUTXOMap.Bytes32ToUTXOMap storage UTXOs = assetIdMapToUTXOSet[assetId];
+        len = UTXOs.length();
+        keys = new bytes32[](len);
+        txids = new bytes32[](len);
+        for (uint256 i = 0; i < len; i++) {
+            (, Types.UTXO memory utxo) = UTXOs.at(i);
+            bytes32 key = keccak256(
+                abi.encodePacked(utxo.txid, utxo.index)
+            );
+            keys[i] = key;
+            txids[i] = utxo.txid;
+        }
+    }
+
+    function getAUTXO(bytes32 assetId, bytes32 key) public view returns (Types.UTXO memory utxo) {
+        EnumerableUTXOMap.Bytes32ToUTXOMap storage UTXOs = assetIdMapToUTXOSet[assetId];
+        utxo = UTXOs.get(key);
+    }
+
+    function removeAUTXO(bytes32 assetId, bytes32 key) public {
+        EnumerableUTXOMap.Bytes32ToUTXOMap storage UTXOs = assetIdMapToUTXOSet[assetId];
+        UTXOs.remove(key);
+    }
+
+    function update(bytes32 assetId, bytes32 txid, Types.Input[] calldata inputs, Types.Output[] calldata outputs) public {
+        _updateUTXOs(assetId, txid, inputs, outputs);
     }
 }
